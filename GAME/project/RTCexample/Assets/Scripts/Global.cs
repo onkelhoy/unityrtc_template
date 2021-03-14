98b076fcabe5277c45747e58445e7376a36681d6
Henry <henry.pap@modis.com>
2021-03-14 02:38:12 +0100

    hm

---
+++ GAME/project/RTCexample/Assets/Scripts/Global.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Global : MonoBehaviour
+{
+    public GameObject PlayerPrefab { get; set; }
+
+    private void Awake()
+    {
+        WEB.onStart += Init;
+    }
+
+    private void Init(StartMessage start)
+    {
+        //Instantiate(PlayerPrefab, );
+        if (WEB.HOST == WEB.ID)
+        {
+            float pie = Mathf.PI * 2 / start.Peers.Length;
+
+            for (int i = 0; i < start.Peers.Length; i++)
+            {
+
+            }
+        }
+    }
+}
